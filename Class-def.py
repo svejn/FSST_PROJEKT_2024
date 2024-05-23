e46be82530afdfb30a9cc40e5661ccca3556b1ee
svejn <svejnoha.martin5@gmail.com>
2024-05-23 11:27:57 +0200

    class 'sensor' with methods defined new

--- Class-def.py
+++ Class-def.py
@@ -1,1 +1,21 @@
-#Projekt code
+# Hier sind die benoetigten Klassen definiert
+
+class Sensor:
+    
+    def __init__(self):
+        pass
+
+    def check(self, sensor1, sensor2, sensor3):
+        pass
+    
+    def read(self, sensor1, sensor2, sensor3):
+        pass
+
+    def calc(self, data1, data2, data3):
+        pass
+
+    def log_data(self, data1, data2, data3):
+        pass
+
+    def log_err(self, sensor1, sensor2, sensor3):
+        pass

d65efc495145117ec51c2f525061de4b2a767bcc
svejn <svejnoha.martin5@gmail.com>
2024-06-06 11:37:19 +0200

    function read defined, test sensor values added

--- class-definition.py
+++ class-definition.py
@@ -4,12 +4,31 @@ class Sensor:
     
     def __init__(self):
         pass
-
-    def check(self, sensor1, sensor2, sensor3):
-        pass
     
     def read(self, sensor1, sensor2, sensor3):
-        pass
+        """
+        def DataTypeError(data):
+            if data == 0:
+                print("datum is zero")
+                pass
+            elif data != int or float:
+                print("wrong datatype; datum must be integer")
+        """    
+        for sensor_num in range(1,4,1):
+            filename = f"sensor{sensor_num}_values.txt"
+            try:
+                with open(filename, "r") as sensor_data:
+                    data = sensor_data.read()
+                    print(data)
+            except FileNotFoundError:
+                print("Your file was not found or doesn't exist.")
+                break
+            """
+            except DataTypeError:
+                DataTypeError(data)
+            """
+
+
 
     def calc(self, data1, data2, data3):
         pass
@@ -19,3 +38,7 @@ class Sensor:
 
     def log_err(self, sensor1, sensor2, sensor3):
         pass
+
+
+s = Sensor()
+s.read("sensor1_values.txt", "sensor2_values.txt", "sensor3_values.txt")

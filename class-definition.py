0b6c7757036a1f4c6b28455683824487ddd13e7f
svejn <svejnoha.martin5@gmail.com>
2024-06-13 11:38:08 +0200

    read()-extendet and optimized. calc()-definiert

--- class-definition.py
+++ class-definition.py
@@ -3,35 +3,38 @@
 class Sensor:
     
     def __init__(self):
-        pass
+        self.data = []
     
     def read(self, sensor1, sensor2, sensor3):
-        """
-        def DataTypeError(data):
-            if data == 0:
-                print("datum is zero")
-                pass
-            elif data != int or float:
-                print("wrong datatype; datum must be integer")
-        """    
-        for sensor_num in range(1,4,1):
+        for sensor_num in range(1, 4):
             filename = f"sensor{sensor_num}_values.txt"
             try:
                 with open(filename, "r") as sensor_data:
-                    data = sensor_data.read()
-                    print(data)
+                    data_r = sensor_data.read().split()
+                    data_r = [int(item) for item in data_r]
+                    self.data.append(data_r)
+                #print(f"Data from {filename}: {data_r}")
+
             except FileNotFoundError:
-                print("Your file was not found or doesn't exist.")
+                print(f"File {filename} was not found or doesn't exist.")
                 break
-            """
-            except DataTypeError:
-                DataTypeError(data)
-            """
+        print(self.data)
 
-
-
-    def calc(self, data1, data2, data3):
-        pass
+    def calc(self):
+        if not self.data:
+            print("No data available to calculate")
+            return None
+        sum_data = 0
+        len_data = 0
+        for sublist in self.data:
+            for item in sublist:
+                sum_data += item
+                len_data += 1
+            #print(sublist)
+        print(sum_data)
+        amw_sensor_data = sum_data / len_data
+        print(f"Arithmetic Mean: {amw_sensor_data}")
+        return amw_sensor_data
 
     def log_data(self, data1, data2, data3):
         pass
@@ -42,3 +45,4 @@ class Sensor:
 
 s = Sensor()
 s.read("sensor1_values.txt", "sensor2_values.txt", "sensor3_values.txt")
+s.calc()
